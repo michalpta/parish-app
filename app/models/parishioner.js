9d744369e16163c60201736f51c92070b8df0e2d
Michal <michal.a.ptaszek@gmail.com>
2016-03-15 18:47:38 +0100

    Rename parishioner model property: 'number' to 'streetNumber' which makes its purpose more obvious

--- app/models/parishioner.js
+++ app/models/parishioner.js
@@ -5,11 +5,11 @@ export default DS.Model.extend({
   name: DS.attr(),
   city: DS.attr(),
   street: DS.attr(),
-  number: DS.attr(),
-  numberSuffix: DS.attr(),
+  streetNumber: DS.attr(),
+  streetNumberSuffix: DS.attr(),
   offerings: DS.hasMany('offering'),
 
   address: Ember.computed('city', 'street', 'number', function() {
-    return `${this.get('city')}, ${this.get('street')} ${this.get('number')}`;
+    return `${this.get('city')}, ${this.get('street')} ${this.get('streetNumber')}`;
   })
 });
