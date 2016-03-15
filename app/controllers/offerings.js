abac7813353a71ebfd1ff98887090638d784c2a8
Michal <michal.a.ptaszek@gmail.com>
2016-03-15 23:24:14 +0100

    Unify input form layouts (parishioners and offerings)

--- app/controllers/offerings.js
+++ app/controllers/offerings.js
@@ -3,8 +3,8 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   isNewOfferingFormVisible: false,
   actions: {
-    toggleShowNewOfferingForm: function() {
-      this.toggleProperty('isNewOfferingFormVisible');
-    }
+    toggleIsFormVisible: function() {
+      this.toggleProperty('isFormVisible');
+    },
   }
 });
