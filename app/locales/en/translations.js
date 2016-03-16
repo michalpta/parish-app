567a41584f33bf75705fd1fb262272057bc3b257
Michal <michal.a.ptaszek@gmail.com>
2016-03-16 19:10:15 +0100

    Complete translation to Polish

--- app/locales/en/translations.js
+++ app/locales/en/translations.js
@@ -11,12 +11,43 @@ export default {
 
   'app.name' : 'Parish App',
   'app.homepage': 'Home',
+  'app.actions': {
+    'save': 'Save',
+    'close': 'Close',
+    'edit': 'Edit'
+  },
+  'app.alerts': {
+    'recordNotSaved': 'This record is not saved yet.',
+    'recordCannotBeSaved': 'This record cannot be saved.',
+    'noRecordsFound': 'No records found.'
+  },
+  'app.labels': {
+    'total': 'Total',
+    'searchPlaceholder': 'Quick search through records...'
+  },
   'parishioner': {
     'name': 'Parishioner',
-    'pluralName': 'Parishioners'
+    'pluralName': 'Parishioners',
+    'actions': {
+      'create': 'Add new parishioner'
+    },
+    'attributes': {
+      'name': 'Name',
+      'address': 'Address',
+      'city': 'City',
+      'street': 'Street',
+      'streetNumber': 'Street number'
+    }
   },
   'offering': {
     'name': 'Offering',
-    'pluralName': 'Offerings'
+    'pluralName': 'Offerings',
+    'actions': {
+      'create' : 'Add new offering'
+    },
+    'attributes': {
+      'date': 'Date',
+      'value': 'Offering'
+    }
   }
 };
--- app/locales/pl/translations.js
+++ app/locales/pl/translations.js
@@ -10,13 +10,44 @@ export default {
   // "key.with.interpolation": "Text with {{anInterpolation}}"
 
   'app.name': 'Aplikacja Parafialna',
-  'app.homepage': 'Strona domowa',
+  'app.homepage': 'Start',
+  'app.actions': {
+    'save': 'Zapisz',
+    'close': 'Zamknij',
+    'edit': 'Edytuj'
+  },
+  'app.alerts': {
+    'recordNotSaved': 'Ten rekord nie jest jeszcze zapisany.',
+    'recordCannotBeSaved': 'Tego rekordu nie można zapisać.',
+    'noRecordsFound': 'Nie znaleziono żadnych rekordów.'
+  },
+  'app.labels': {
+    'total': 'Razem',
+    'searchPlaceholder': 'Szybko przeszukaj rekordy...'
+  },
   'parishioner': {
     'name': 'Parafianin',
-    'pluralName': 'Parafianie'
+    'pluralName': 'Parafianie',
+    'actions': {
+      'create': 'Dodaj nowego parafianina'
+    },
+    'attributes': {
+      'name': 'Nazwisko',
+      'address': 'Adres',
+      'city': 'Miejscowość',
+      'street': 'Ulica',
+      'streetNumber': 'Numer domu'
+    }
   },
   'offering': {
     'name': 'Ofiara',
-    'pluralName': 'Ofiary'
+    'pluralName': 'Ofiary',
+    'actions': {
+      'create' : 'Dodaj nową ofiarę'
+    },
+    'attributes': {
+      'date': 'Data',
+      'value': 'Ofiara'
+    }
   }
 };
