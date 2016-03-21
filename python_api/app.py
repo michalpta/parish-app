61c7f679455fade87f013ad65a329c55440ed9b3
Michal Ptaszek <michal.ptaszek@capgemini.com>
2016-03-22 00:10:34 +0100

    Create Full Rest API for Parish  App in Flask

--- python_api/app.py
+++ python_api/app.py
@@ -15,12 +15,12 @@ def index(path):
 
 class Parishioner(db.Model):
   id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String)
+  name = db.Column(db.String, nullable=False)
   city = db.Column(db.String)
   street = db.Column(db.String)
   streetNumber = db.Column(db.Integer)
   streetNumberSuffix = db.Column(db.String)
-  offerings = db.relationship('Offering', backref='parishioner', lazy='dynamic')
+  offerings = db.relationship('Offering', backref='parishioner', lazy='dynamic', cascade="all, delete-orphan")
 
   @property
   def serialize(self):
@@ -41,16 +41,16 @@ class Parishioner(db.Model):
 
 class Offering(db.Model):
   id = db.Column(db.Integer, primary_key=True)
-  value = db.Column(db.Float)
-  date = db.Column(db.DateTime)
-  parishioner_id = db.Column(db.Integer, db.ForeignKey('parishioner.id'))
+  value = db.Column(db.Float, nullable=False)
+  date = db.Column(db.DateTime, nullable=False)
+  parishioner_id = db.Column(db.Integer, db.ForeignKey('parishioner.id'), nullable=False)
 
   @property
   def serialize(self):
         return {
             'id': self.id,
             'value': self.value,
-            'date': self.date,
+            'date': datetime.strftime(self.date,'%Y-%m-%d %H:%M'),
             'parishioner': self.parishioner_id
         }
 
@@ -62,17 +62,28 @@ class Offering(db.Model):
 # API
 class Parishioners(Resource):
   def get(self):
-        return jsonify({'parishioner': [i.serialize for i in Parishioner.query.all()]})
+      return jsonify({'parishioner': [i.serialize for i in Parishioner.query.all()]})
   def post(self):
-        data = request.json['parishioner']
-        i = Parishioner(data)
-        db.session.add(i)
-        db.session.commit()
-        return 200
+      data = request.json['parishioner']
+      i = Parishioner(data)
+      db.session.add(i)
+      db.session.commit()
+      return jsonify({'parishioner': i.serialize})
 
 class ParishionersWithId(Resource):
   def get(self, parishioner_id):
       return jsonify({'parishioner': Parishioner.query.get(parishioner_id).serialize})
+  def delete(self, parishioner_id):
+      parishioner = Parishioner.query.get(parishioner_id)
+      db.session.delete(parishioner)
+      db.session.commit()
+      return
+  def put(self, parishioner_id):
+      data = request.json['parishioner']
+      parishioner = Parishioner.query.get(parishioner_id)
+      parishioner.name = data['name']
+      db.session.commit()
+      return jsonify({'parishioner': parishioner.serialize})
 
 class Offerings(Resource):
   def get(self):
@@ -82,11 +93,24 @@ class Offerings(Resource):
       i = Offering(data)
       db.session.add(i)
       db.session.commit()
-      return 200
+      return jsonify({'offering': i.serialize})
 
 class OfferingsWithId(Resource):
   def get(self, offering_id):
       return jsonify({'offering': Offering.query.get(offering_id).serialize})
+  def delete(self, offering_id):
+      offering = Offering.query.get(offering_id)
+      db.session.delete(offering)
+      db.session.commit()
+      return
+  def put(self, offering_id):
+      data = request.json['offering']
+      offering = Offering.query.get(offering_id)
+      offering.value = data['value']
+      offering.date = datetime.strptime(data['date'],'%Y-%m-%d %H:%M')
+      offering.parishioner_id = data['parishioner']
+      db.session.commit()
+      return jsonify({'offering': offering.serialize})
 
 api.add_resource(Parishioners, '/parishioners')
 api.add_resource(ParishionersWithId, '/parishioners/<int:parishioner_id>')
