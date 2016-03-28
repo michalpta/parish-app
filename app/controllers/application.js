6496d6a3383c850ab33f30e30db9e67f8f06ee6f
Michal Ptaszek <michal.ptaszek@capgemini.com>
2016-03-28 12:15:36 +0200

    Hide application outlet when not authenticated

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,8 +1,13 @@
 import Ember from 'ember';
+import ENV from 'parish-app/config/environment';
 
 export default Ember.Controller.extend({
   i18n: Ember.inject.service(),
   setLocale: function(locale) {
     this.set('i18n.locale', locale);
-  }
+  },
+  isAuthenticated: Ember.computed('session.isAuthenticated', function(){
+    return this.get('session.isAuthenticated') ||
+      !ENV.useFirebase;
+  })
 });
